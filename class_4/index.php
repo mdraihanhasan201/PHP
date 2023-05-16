abbb169f75c7a3ab9b88923aa60512debe381d4c
md rayhan islam <111277475+mdraihanhasan201@users.noreply.github.com>
2023-05-16 20:23:36 +0600

    Add files via upload
    basic of php

---
+++ class_4/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <?php
+            $first_name="rayhan";
+            $last_name="tushar";
+
+            ?>
+
+
+       <h1>
+        hello my first name is:<?php echo $first_name ?> and my last name is: <?php echo $last_name ?> 
+       </h1>
+
+
+
+        </body>
+        </html>
+            
